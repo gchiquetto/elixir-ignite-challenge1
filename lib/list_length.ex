888893a6cf3a8b6711d806e9615ba01369baaf54
Gabriela Chiquetto <74207793+gchiquetto@users.noreply.github.com>
2023-01-26 17:14:02 +0100

    Added challenge solution

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    sum(list, 0)
+  end
+
+  defp sum([], acc) do
+    acc
+  end
+  defp sum([ _ | tail], acc) do
+    acc = acc + 1
+    sum(tail, acc)
   end
 end
